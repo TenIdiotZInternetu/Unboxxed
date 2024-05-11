17e5040a67ea64a8e66a344c6f21111ff7319038
TIZI <tenidiotzinternetudefault@gmail.com>
2024-05-11 22:48:01 +0200

    Added Grounded Jumping and Falling state to PlayerCController

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using PlayerScripts;
@@ -7,16 +8,27 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private float horizontalSpeed = 1.0f;
-    [SerializeField] private float jumpSpeed = 1.0f;
+    [SerializeField] private float jumpForce = 1.0f;
+    [SerializeField] private float maxJumpHeight = 3.0f;
 
+    private Transform _transform;
     private Rigidbody2D _rigidBody;
+    private LayerMask _groundLayer;
     
-    private Vector2 _up = Vector2.up; 
+    private Vector2 _up = Vector2.up;
+
+    private bool _isJumping;
+    private bool _isGrounded;
+    private float _jumpHeight;
     
+    private bool IsFalling => _rigidBody.velocity.y < 0;
+
     // Start is called before the first frame update
     void Start()
     {
+        _transform = transform;
         _rigidBody = GetComponent<Rigidbody2D>();
+        _groundLayer = LayerMask.NameToLayer("Ground");
         
         Controls.Jumps += Jump;
         Controls.JumpsRelease += CancelJump;
@@ -27,7 +39,7 @@ public class PlayerController : MonoBehaviour
     {
         MoveHorizontal(Controls.MoveHorizontal);
     }
-    
+
     private void MoveHorizontal(float value)
     {
         _rigidBody.velocity = new Vector2(value * horizontalSpeed, _rigidBody.velocity.y);
@@ -35,11 +47,32 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        _rigidBody.AddForce(jumpSpeed * _up);
+        if (!_isJumping && !_isGrounded) return;
+
+        _isJumping = true;
+        
+        _rigidBody.AddForce(jumpForce * _up);
+        _jumpHeight += _rigidBody.velocity.y * Time.deltaTime;
+
+        if (_jumpHeight >= maxJumpHeight)
+        {
+            _isJumping = false;
+            _jumpHeight = 0;
+        }
     }
 
-    private void CancelJump()
+    public void CancelJump()
     {
+        if (!_isJumping) return;
         
+        _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, 0);
+        
+        _isJumping = false;
+        _jumpHeight = 0;
+    }
+
+    public void Ground(bool isGrounded)
+    {
+        _isGrounded = isGrounded;
     }
 }
--- Assets/Scripts/Trigger.cs
+++ Assets/Scripts/Trigger.cs
@@ -1,6 +1,9 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting.Dependencies.NCalc;
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -9,19 +12,24 @@ public class Trigger : MonoBehaviour
     public UnityEvent onTriggerEnter;
     public UnityEvent onTriggerExit;
 
-    [SerializeField] private List<LayerMask> _acceptedLayers;
+    [SerializeField] private LayerMask acceptedLayers;
+
 
     private void OnTriggerEnter2D(Collider2D col)
     {
-        if (!_acceptedLayers.Contains(col.gameObject.layer)) return;
-        
+        if (!IsOnLayer(col)) return;
         onTriggerEnter.Invoke();
     }
     
     private void OnTriggerExit2D(Collider2D col)
     {
-        if (!_acceptedLayers.Contains(col.gameObject.layer)) return;
-        
+        if (!IsOnLayer(col)) return;
         onTriggerExit.Invoke();
+    }
+
+    private bool IsOnLayer(Collider2D collider)
+    {
+        int layerNumber = collider.gameObject.layer;
+        return (1 << layerNumber & acceptedLayers) != 0;
     }
 }
