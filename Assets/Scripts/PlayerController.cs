a0e8e6c0b9a841fb4e4c6605d9bcf239c020eb5f
TIZI <tenidiotzinternetudefault@gmail.com>
2024-05-12 15:42:11 +0200

    Did some work on PlayerController, will be changed immediately

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,7 +2,9 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using PlayerScripts;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [RequireComponent(typeof(Rigidbody2D))]
 public class PlayerController : MonoBehaviour
@@ -10,16 +12,23 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float horizontalSpeed = 1.0f;
     [SerializeField] private float jumpForce = 1.0f;
     [SerializeField] private float maxJumpHeight = 3.0f;
+    
+    [SerializeField] private float gravityCancelTime = 0.3f;
+    [SerializeField] private float fallingSpeed = 5.0f;
+    [SerializeField] private float jumpCancelForce = 3.0f;
 
     private Transform _transform;
     private Rigidbody2D _rigidBody;
-    private LayerMask _groundLayer;
     
     private Vector2 _up = Vector2.up;
 
     private bool _isJumping;
     private bool _isGrounded;
+    private bool _inJumpPeak;
+    
+    private float _airTime;
     private float _jumpHeight;
+    private float _prevYPos;
     
     private bool IsFalling => _rigidBody.velocity.y < 0;
 
@@ -28,7 +37,6 @@ public class PlayerController : MonoBehaviour
     {
         _transform = transform;
         _rigidBody = GetComponent<Rigidbody2D>();
-        _groundLayer = LayerMask.NameToLayer("Ground");
         
         Controls.Jumps += Jump;
         Controls.JumpsRelease += CancelJump;
@@ -38,6 +46,40 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         MoveHorizontal(Controls.MoveHorizontal);
+        ApplyGravity();
+        CheckJumpHeight();
+    }
+
+    private void CheckJumpHeight()
+    {
+        if (_isJumping)
+        {
+            float deltaHeight = _transform.position.y - _prevYPos;
+            _jumpHeight += deltaHeight;
+            
+            if (_jumpHeight >= maxJumpHeight)
+            {
+                CancelJump();
+            }
+        }
+        
+        _prevYPos = _transform.position.y;
+    }
+
+    private void ApplyGravity()
+    {
+        if (IsFalling)
+        {
+            _rigidBody.gravityScale = fallingSpeed;
+        }
+        else if (_inJumpPeak && _airTime < gravityCancelTime)
+        {
+            _rigidBody.gravityScale = 0.1f;
+        }
+        else
+        {
+            _rigidBody.gravityScale = 1;
+        }
     }
 
     private void MoveHorizontal(float value)
@@ -47,32 +89,26 @@ public class PlayerController : MonoBehaviour
 
     private void Jump()
     {
-        if (!_isJumping && !_isGrounded) return;
+        if (!_isGrounded || _isJumping) return;
 
         _isJumping = true;
-        
-        _rigidBody.AddForce(jumpForce * _up);
-        _jumpHeight += _rigidBody.velocity.y * Time.deltaTime;
-
-        if (_jumpHeight >= maxJumpHeight)
-        {
-            _isJumping = false;
-            _jumpHeight = 0;
-        }
+        _rigidBody.AddForce(jumpForce * _up, ForceMode2D.Impulse);
     }
 
     public void CancelJump()
     {
         if (!_isJumping) return;
         
-        _rigidBody.velocity = new Vector2(_rigidBody.velocity.x, 0);
+        _rigidBody.AddForce(_rigidBody.velocity.y * jumpCancelForce * Vector2.down, ForceMode2D.Impulse);
         
         _isJumping = false;
+        _inJumpPeak = true;
         _jumpHeight = 0;
     }
 
     public void Ground(bool isGrounded)
     {
         _isGrounded = isGrounded;
+        _airTime = 0;
     }
 }
