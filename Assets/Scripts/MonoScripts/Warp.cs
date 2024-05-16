80d9dcfbc27740fd9da53a9a56bf123722682d62
TIZI <tenidiotzinternetudefault@gmail.com>
2024-05-16 23:24:14 +0200

    Repurposed Warp script to be used on WarpHeads.

--- Assets/Scripts/MonoScripts/Warp.cs
+++ Assets/Scripts/MonoScripts/Warp.cs
@@ -1,11 +1,17 @@
+using SOs;
 using UnityEngine;
 
 namespace MonoScripts
 {
     public class Warp : MonoBehaviour
     {
-        public void WarpHere(GameObject obj) {
-            obj.transform.position = transform.position;
+        [SerializeField] private BoxController box;
+        [SerializeField] private Transform warpTarget;
+        [SerializeField] private GravityDirectionSo gravityDirection;
+        
+        public void WarpToBox(GameObject obj) {
+            obj.transform.position = warpTarget.position;
+            box.Enter(gravityDirection);
         }
     }
 }
