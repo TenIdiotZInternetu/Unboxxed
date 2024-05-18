0e89712f1dce047fb2ba0c3eb88e673658dff699
TIZI <tenidiotzinternetudefault@gmail.com>
2024-05-18 23:16:32 +0200

    Made TimerUI to specify the TMP component. Removed hours property. Made Elapsedtime to count time in milliseconds.

--- Assets/Scripts/MonoScripts/UI/TimerUI.cs
+++ Assets/Scripts/MonoScripts/UI/TimerUI.cs
@@ -1,36 +1,29 @@
 using System;
 using TMPro;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 namespace MonoScripts.Menus
 {
-    [RequireComponent(typeof(TMP_Text))]
     public class TimerUI : MonoBehaviour
+
     {
+        [SerializeField] private TMP_Text tmpText;
         [SerializeField] private string format;
 
         public float ElapsedTime { get; private set; }
 
         public float TotalSeconds => ElapsedTime / 1000;
         public float TotalMinutes => TotalSeconds / 60;
-        public float TotalHours => TotalMinutes / 60;
         
-        public float Milliseconds => ElapsedTime % 1000;
-        public float Seconds => TotalSeconds % 60;
-        public float Minutes => TotalMinutes % 60;
-        
-
-        private TMP_Text _text;
-
-        private void Awake()
-        {
-            _text = GetComponent<TMP_Text>();
-        }
+        public int Milliseconds => (int)(ElapsedTime % 1000);
+        public int Seconds => (int)(TotalSeconds % 60);
+        public int Minutes => (int)(TotalMinutes % 60);
 
         void Update()
         {
-            ElapsedTime += Time.deltaTime;
-            _text.text = string.Format(format, TotalHours, Minutes, Seconds, Milliseconds);
+            ElapsedTime += Time.deltaTime * 1000;
+            tmpText.text = string.Format(format, Minutes, Seconds, Milliseconds);
         }
 
         public void Reset()
