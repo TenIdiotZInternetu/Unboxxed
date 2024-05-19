4d55c110e9ceece575fee48fbd4fcc31232af072
TIZI <tenidiotzinternetudefault@gmail.com>
2024-05-19 23:08:28 +0200

    Created option for Trigger to be single use only.

--- Assets/Scripts/MonoScripts/LevelObjects/Trigger.cs
+++ Assets/Scripts/MonoScripts/LevelObjects/Trigger.cs
@@ -9,18 +9,24 @@ namespace MonoScripts.LevelObjects
         public UnityEvent<GameObject> onTriggerExit;
 
         [SerializeField] private LayerMask acceptedLayers;
+        [SerializeField] private bool singleUse;
 
+        private bool _used;
 
         private void OnTriggerEnter2D(Collider2D col)
         {
+            if (_used && singleUse) return;
             if (!IsOnLayer(col)) return;
             onTriggerEnter.Invoke(col.gameObject);
         }
     
         private void OnTriggerExit2D(Collider2D col)
         {
+            if (_used && singleUse) return;
             if (!IsOnLayer(col)) return;
             onTriggerExit.Invoke(col.gameObject);
+
+            _used = true;
         }
 
         private bool IsOnLayer(Collider2D collider)
