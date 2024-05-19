8b40d84284d4074b8ad949834b8f791776a22a4b
TIZI <tenidiotzinternetudefault@gmail.com>
2024-05-19 23:20:49 +0200

    Renamed RotateToGravity to Rotation, and expanded it accept any angle/quaternion.

--- Assets/Scripts/MonoScripts/Animations/Rotation.cs
+++ Assets/Scripts/MonoScripts/Animations/Rotation.cs
@@ -4,17 +4,20 @@ using MonoScripts.SceneControllers;
 using Unity.VisualScripting;
 using UnityEngine;
 
-public class RotateToGravity : MonoBehaviour
+public class Rotation : MonoBehaviour
 {
     private const float FPS = 30f;
     
     [SerializeField] private AnimationCurve rotationCurve;
     [SerializeField] private float rotationDuration = 1f;
     
+    [SerializeField] private bool rotateToGravity = true;
+    
     private GravityController _gravity;
     private Transform _transform;
     
     private Quaternion _currentRotation;
+    private Quaternion _targetRotation;
     private float _elapsedTime;
     
     private void Awake()
@@ -24,11 +27,39 @@ public class RotateToGravity : MonoBehaviour
         
         _transform.rotation = _gravity.RotationQuaternion;
         _currentRotation = _transform.rotation;
-        _gravity.OnGravityChanged += Rotate;
+
+        if (rotateToGravity)
+        {
+            _gravity.OnGravityChanged += RotateToGravity;
+        }
+    }
+
+    public void RotateToGravity()
+    {
+        RotateTo(_gravity.RotationQuaternion);
     }
     
-    private void Rotate()
+    public void RotateTo(float targetDegrees)
     {
+        _targetRotation = Quaternion.Euler(0, 0, targetDegrees);
+        StartCoroutine(RotateCoroutine());
+    }
+    
+    public void RotateTo(Quaternion targetRotation)
+    {
+        _targetRotation = targetRotation;
+        StartCoroutine(RotateCoroutine());
+    }
+    
+    public void RotateBy(float degrees)
+    {
+        _targetRotation = _currentRotation * Quaternion.Euler(0, 0, degrees);
+        StartCoroutine(RotateCoroutine());
+    }
+    
+    public void RotateBy(Quaternion rotation)
+    {
+        _targetRotation = _currentRotation * rotation;
         StartCoroutine(RotateCoroutine());
     }
 
@@ -40,7 +71,7 @@ public class RotateToGravity : MonoBehaviour
             float timeProgress = _elapsedTime / rotationDuration;
             float rotationProgress = rotationCurve.Evaluate(timeProgress);
             
-            _transform.rotation = Quaternion.Slerp(_currentRotation, _gravity.RotationQuaternion, rotationProgress);
+            _transform.rotation = Quaternion.Slerp(_currentRotation, _targetRotation, rotationProgress);
             yield return null;
         }
         
